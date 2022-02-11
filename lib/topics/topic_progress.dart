710a6af96480a2c1cfd5886a5885fbdd3da5dcaa
florvhee <florvhee@gmail.com>
2022-02-11 16:23:46 +0100

    fix bug topic progress

--- lib/topics/topic_progress.dart
+++ lib/topics/topic_progress.dart
@@ -13,20 +13,22 @@ class TopicProgress extends StatelessWidget {
     Report? report = Provider.of<Report?>(context);
     return Row(
       children: [
-        _progressCount(report!, topic),
-        /* Expanded(
+        _progressCount(report, topic),
+        Expanded(
           child:
               AnimatedProgressbar(value: report != null ? _calculateProgress(topic, report) : 0, height: 8),
-        ), */
+        ),
       ],
     );
   }
 
-  Widget _progressCount(Report report, Topic topic) {
+  Widget _progressCount(Report? report, Topic topic) {
     return Padding(
       padding: const EdgeInsets.only(left: 8),
       child: Text(
-        '${report != null && report.topics[topic.id]?.length ?? 0} / ${topic.quizzes.length}',
+        report != null
+            ? '${report.topics[topic.id]?.length ?? 0} / ${topic.quizzes.length}'
+            : '0 / ${topic.quizzes.length}',
         style: const TextStyle(fontSize: 10, color: Colors.grey),
       ),
     );
